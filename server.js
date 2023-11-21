16e063f27fc3d7f3dcd8cdc613b757d6943cd1c4
Gileadejls <gileadelima16@gmail.com>
2023-11-21 12:35:45 -0300

    adicionando models e implementando conexão ao banco de dados

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 require('dotenv').config()
 
+// DEPENDENCIAS
 const express = require('express')
 const path = require('path')
 const app = express()
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+
+//IMPORTANDO MODEL
+const Login = require('./src/models/loginModel')
 
 mongoose.connect(process.env.CONNECTIONSTRING).then(()=>{
     console.log("Conectado ao bando de dados MONGODB")
@@ -17,7 +22,7 @@ app.set('view engine', 'ejs')
 //ROTA CSS
 app.use(express.static(path.join(__dirname, 'public')));
 
-//ROTAS
+//ROTAS ---------------------------------------------------------------------
 
 //HOME
 app.get('/', (req, res)=>{
@@ -27,6 +32,22 @@ app.get('/', (req, res)=>{
 //LOGIN
 app.get('/login', (req, res)=>{
     res.render('login')
+})
+
+//HABILITANDO O JSON
+app.use(bodyParser.urlencoded({extended: true}))
+
+//LOGIN POST
+app.post('/login', async (req, res)=>{
+    const loginRequest = new Login(req.body)
+    const result = await loginRequest.logar()
+    if(!result){
+        console.log(`Usuario Invalido ${loginRequest.errors}`)
+        return
+    }
+
+    console.log(`Login Efetuado com Sucesso! USUARIO: ${loginRequest.user}`)
+    res.render('index')
 })
 
 //INICIANDO SERVIDOR
---
+++ src/models/loginModel.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose')
+
+const loginSchema = mongoose.Schema({
+    nome: String,
+    password: String,
+})
+
+const userModel = mongoose.model('usuarios', loginSchema)
+
+
+class Login{
+    constructor(body){
+        this.body = body
+        this.errors = []
+        this.user = null
+    }
+
+    async logar(){
+        this.validate()
+
+        if(this.errors.length == 0){
+            return
+        }
+
+        try{
+            let result = await userModel.find({nome: this.body.name, password: this.body.password})
+            if(result.length == 0){
+                this.errors.push('Não foi possivel fazer login, tente novamente!')
+                return 
+            }
+
+            this.user = result[0]._id
+            return true
+        }catch(e){console.log(e)}
+    }
+
+
+    validate(){
+        if(this.body.name.length < 4 || this.body.name.length || this.body.name == undefined || this.body.name.length == null){
+            this.errors.push('Login ou senha invalidos, por favor tente novamente.')
+            return
+        }
+    }
+}
+
+
+module.exports = Login
